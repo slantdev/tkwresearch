2eedf3165ee2d88c283632a3ca0a0433f742bfb2
Fransnico Susanto <fransnico@amazeworx.com>
2023-05-11 06:31:52 +0700

    single

--- template-parts/layouts/single-header.php
+++ template-parts/layouts/single-header.php
@@ -3,7 +3,7 @@
  * Single Page Header
  */
 ?>
-<section class="relative min-h-[60vh] flex flex-col justify-end bg-brand-bluedark">
+<section class="relative xl:min-h-[60vh] flex flex-col justify-end bg-brand-bluedark">
   <div class="absolute inset-0 z-0">
     <div class="absolute inset-0 mix-blend-multiply bg-black/50"></div>
     <?php
@@ -13,17 +13,17 @@
     }
     ?>
   </div>
-  <div class="container max-w-screen-xl relative z-10 pt-[300px] pb-20 h-full">
-    <div class="flex h-full items-center text-white">
-      <div class="w-1/2">
-        <h1 class="font-bold text-[50px] leading-tight"><?php echo get_the_title() ?></h1>
-        <div class="text-lg mt-6"><time datetime="<?php echo get_the_date('c'); ?>" itemprop="datePublished" class="text-lg"><?php echo get_the_date(); ?></time></div>
+  <div class="container max-w-screen-xl relative z-10 pt-52 xl:pt-[216px] pb-16 xl:pb-10 h-full">
+    <div class="flex flex-col md:flex-row h-full items-center text-white">
+      <div class="w-full max-w-prose">
+        <h1 class="font-bold text-[40px] lg:text-[50px] leading-tight"><?php echo get_the_title() ?></h1>
+        <div class="mt-4 lg:mt-6 text-base lg:text-lg"><time datetime="<?php echo get_the_date('c'); ?>" itemprop="datePublished" class="text-lg"><?php echo get_the_date(); ?></time></div>
       </div>
     </div>
   </div>
 </section>
 
-<div class="bg-brand-graylight py-6">
+<div class="bg-brand-beigelight py-6">
   <div class="container max-w-screen-xl">
     <?php
     if (function_exists('yoast_breadcrumb')) {
--- template-parts/single/content-single.php
+++ template-parts/single/content-single.php
@@ -1,6 +1,6 @@
-<article id="post-<?php the_ID(); ?>" <?php post_class('py-12 xl:py-20'); ?>>
+<article id="post-<?php the_ID(); ?>" <?php post_class('py-8 lg:py-12 xl:py-20'); ?>>
   <div class="flex flex-wrap lg:flex-nowrap lg:gap-x-8 xl:gap-x-36">
-    <div class="w-2/3">
+    <div class="w-full lg:w-2/3">
       <div class="prose max-w-none xl:prose-lg">
         <?php the_content(); ?>
       </div>

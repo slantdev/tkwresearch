9efaaf2853d2fccab5e1eeca509b723332bef9de
Fransnico Susanto <fransnico@amazeworx.com>
2023-05-11 13:24:27 +0700

    remove screenshots

--- inc/acf.php
+++ inc/acf.php
@@ -43,128 +43,128 @@ function acf_icon_path_suffix($path_suffix)
  * @array       $field      Field settings
  * @array       $layout     Layout settings
  */
-add_filter('acfe/flexible/thumbnail/layout=benefits', 'benefits_thumbnail', 10, 3);
-function benefits_thumbnail($thumbnail, $field, $layout)
-{
-  return get_stylesheet_directory_uri() . '/assets/images/layouts/benefits.jpg';
-}
+// add_filter('acfe/flexible/thumbnail/layout=benefits', 'benefits_thumbnail', 10, 3);
+// function benefits_thumbnail($thumbnail, $field, $layout)
+// {
+//   return get_stylesheet_directory_uri() . '/assets/images/layouts/benefits.jpg';
+// }
 
-add_filter('acfe/flexible/thumbnail/layout=case_studies', 'case_studies_thumbnail', 10, 3);
-function case_studies_thumbnail($thumbnail, $field, $layout)
-{
-  return get_stylesheet_directory_uri() . '/assets/images/layouts/case_studies.jpg';
-}
+// add_filter('acfe/flexible/thumbnail/layout=case_studies', 'case_studies_thumbnail', 10, 3);
+// function case_studies_thumbnail($thumbnail, $field, $layout)
+// {
+//   return get_stylesheet_directory_uri() . '/assets/images/layouts/case_studies.jpg';
+// }
 
-add_filter('acfe/flexible/thumbnail/layout=contact_center', 'contact_center_thumbnail', 10, 3);
-function contact_center_thumbnail($thumbnail, $field, $layout)
-{
-  return get_stylesheet_directory_uri() . '/assets/images/layouts/contact_center.jpg';
-}
+// add_filter('acfe/flexible/thumbnail/layout=contact_center', 'contact_center_thumbnail', 10, 3);
+// function contact_center_thumbnail($thumbnail, $field, $layout)
+// {
+//   return get_stylesheet_directory_uri() . '/assets/images/layouts/contact_center.jpg';
+// }
 
-add_filter('acfe/flexible/thumbnail/layout=contact_form_1', 'contact_form_1_thumbnail', 10, 3);
-function contact_form_1_thumbnail($thumbnail, $field, $layout)
-{
-  return get_stylesheet_directory_uri() . '/assets/images/layouts/contact_form_1.jpg';
-}
+// add_filter('acfe/flexible/thumbnail/layout=contact_form_1', 'contact_form_1_thumbnail', 10, 3);
+// function contact_form_1_thumbnail($thumbnail, $field, $layout)
+// {
+//   return get_stylesheet_directory_uri() . '/assets/images/layouts/contact_form_1.jpg';
+// }
 
-add_filter('acfe/flexible/thumbnail/layout=contact_form_2', 'contact_form_2_thumbnail', 10, 3);
-function contact_form_2_thumbnail($thumbnail, $field, $layout)
-{
-  return get_stylesheet_directory_uri() . '/assets/images/layouts/contact_form_2.jpg';
-}
+// add_filter('acfe/flexible/thumbnail/layout=contact_form_2', 'contact_form_2_thumbnail', 10, 3);
+// function contact_form_2_thumbnail($thumbnail, $field, $layout)
+// {
+//   return get_stylesheet_directory_uri() . '/assets/images/layouts/contact_form_2.jpg';
+// }
 
-add_filter('acfe/flexible/thumbnail/layout=cta', 'cta_thumbnail', 10, 3);
-function cta_thumbnail($thumbnail, $field, $layout)
-{
-  return get_stylesheet_directory_uri() . '/assets/images/layouts/cta.jpg';
-}
+// add_filter('acfe/flexible/thumbnail/layout=cta', 'cta_thumbnail', 10, 3);
+// function cta_thumbnail($thumbnail, $field, $layout)
+// {
+//   return get_stylesheet_directory_uri() . '/assets/images/layouts/cta.jpg';
+// }
 
-add_filter('acfe/flexible/thumbnail/layout=cta_2', 'cta_2_thumbnail', 10, 3);
-function cta_2_thumbnail($thumbnail, $field, $layout)
-{
-  return get_stylesheet_directory_uri() . '/assets/images/layouts/cta_2.jpg';
-}
+// add_filter('acfe/flexible/thumbnail/layout=cta_2', 'cta_2_thumbnail', 10, 3);
+// function cta_2_thumbnail($thumbnail, $field, $layout)
+// {
+//   return get_stylesheet_directory_uri() . '/assets/images/layouts/cta_2.jpg';
+// }
 
-add_filter('acfe/flexible/thumbnail/layout=download_lead_gen', 'download_lead_gen_thumbnail', 10, 3);
-function download_lead_gen_thumbnail($thumbnail, $field, $layout)
-{
-  return get_stylesheet_directory_uri() . '/assets/images/layouts/download_lead_gen.jpg';
-}
+// add_filter('acfe/flexible/thumbnail/layout=download_lead_gen', 'download_lead_gen_thumbnail', 10, 3);
+// function download_lead_gen_thumbnail($thumbnail, $field, $layout)
+// {
+//   return get_stylesheet_directory_uri() . '/assets/images/layouts/download_lead_gen.jpg';
+// }
 
-add_filter('acfe/flexible/thumbnail/layout=featured_posts_slider', 'featured_posts_slider_thumbnail', 10, 3);
-function featured_posts_slider_thumbnail($thumbnail, $field, $layout)
-{
-  return get_stylesheet_directory_uri() . '/assets/images/layouts/featured_posts_slider.jpg';
-}
+// add_filter('acfe/flexible/thumbnail/layout=featured_posts_slider', 'featured_posts_slider_thumbnail', 10, 3);
+// function featured_posts_slider_thumbnail($thumbnail, $field, $layout)
+// {
+//   return get_stylesheet_directory_uri() . '/assets/images/layouts/featured_posts_slider.jpg';
+// }
 
-add_filter('acfe/flexible/thumbnail/layout=features_grid', 'features_grid_thumbnail', 10, 3);
-function features_grid_thumbnail($thumbnail, $field, $layout)
-{
-  return get_stylesheet_directory_uri() . '/assets/images/layouts/features_grid.jpg';
-}
+// add_filter('acfe/flexible/thumbnail/layout=features_grid', 'features_grid_thumbnail', 10, 3);
+// function features_grid_thumbnail($thumbnail, $field, $layout)
+// {
+//   return get_stylesheet_directory_uri() . '/assets/images/layouts/features_grid.jpg';
+// }
 
-add_filter('acfe/flexible/thumbnail/layout=hero_slider', 'hero_slider_thumbnail', 10, 3);
-function hero_slider_thumbnail($thumbnail, $field, $layout)
-{
-  return get_stylesheet_directory_uri() . '/assets/images/layouts/hero_slider.jpg';
-}
+// add_filter('acfe/flexible/thumbnail/layout=hero_slider', 'hero_slider_thumbnail', 10, 3);
+// function hero_slider_thumbnail($thumbnail, $field, $layout)
+// {
+//   return get_stylesheet_directory_uri() . '/assets/images/layouts/hero_slider.jpg';
+// }
 
-add_filter('acfe/flexible/thumbnail/layout=image_left_text', 'image_left_text_thumbnail', 10, 3);
-function image_left_text_thumbnail($thumbnail, $field, $layout)
-{
-  return get_stylesheet_directory_uri() . '/assets/images/layouts/image_left_text.jpg';
-}
+// add_filter('acfe/flexible/thumbnail/layout=image_left_text', 'image_left_text_thumbnail', 10, 3);
+// function image_left_text_thumbnail($thumbnail, $field, $layout)
+// {
+//   return get_stylesheet_directory_uri() . '/assets/images/layouts/image_left_text.jpg';
+// }
 
-add_filter('acfe/flexible/thumbnail/layout=info_cards', 'info_cards_thumbnail', 10, 3);
-function info_cards_thumbnail($thumbnail, $field, $layout)
-{
-  return get_stylesheet_directory_uri() . '/assets/images/layouts/info_cards.jpg';
-}
+// add_filter('acfe/flexible/thumbnail/layout=info_cards', 'info_cards_thumbnail', 10, 3);
+// function info_cards_thumbnail($thumbnail, $field, $layout)
+// {
+//   return get_stylesheet_directory_uri() . '/assets/images/layouts/info_cards.jpg';
+// }
 
-add_filter('acfe/flexible/thumbnail/layout=logo_carousel', 'logo_carousel_thumbnail', 10, 3);
-function logo_carousel_thumbnail($thumbnail, $field, $layout)
-{
-  return get_stylesheet_directory_uri() . '/assets/images/layouts/logo_carousel.jpg';
-}
+// add_filter('acfe/flexible/thumbnail/layout=logo_carousel', 'logo_carousel_thumbnail', 10, 3);
+// function logo_carousel_thumbnail($thumbnail, $field, $layout)
+// {
+//   return get_stylesheet_directory_uri() . '/assets/images/layouts/logo_carousel.jpg';
+// }
 
-add_filter('acfe/flexible/thumbnail/layout=resources_grid', 'resources_grid_thumbnail', 10, 3);
-function resources_grid_thumbnail($thumbnail, $field, $layout)
-{
-  return get_stylesheet_directory_uri() . '/assets/images/layouts/resources_grid.jpg';
-}
+// add_filter('acfe/flexible/thumbnail/layout=resources_grid', 'resources_grid_thumbnail', 10, 3);
+// function resources_grid_thumbnail($thumbnail, $field, $layout)
+// {
+//   return get_stylesheet_directory_uri() . '/assets/images/layouts/resources_grid.jpg';
+// }
 
-add_filter('acfe/flexible/thumbnail/layout=resources', 'resources_thumbnail', 10, 3);
-function resources_thumbnail($thumbnail, $field, $layout)
-{
-  return get_stylesheet_directory_uri() . '/assets/images/layouts/resources.jpg';
-}
+// add_filter('acfe/flexible/thumbnail/layout=resources', 'resources_thumbnail', 10, 3);
+// function resources_thumbnail($thumbnail, $field, $layout)
+// {
+//   return get_stylesheet_directory_uri() . '/assets/images/layouts/resources.jpg';
+// }
 
-add_filter('acfe/flexible/thumbnail/layout=solution_cards', 'solution_cards_thumbnail', 10, 3);
-function solution_cards_thumbnail($thumbnail, $field, $layout)
-{
-  return get_stylesheet_directory_uri() . '/assets/images/layouts/solution_cards.jpg';
-}
+// add_filter('acfe/flexible/thumbnail/layout=solution_cards', 'solution_cards_thumbnail', 10, 3);
+// function solution_cards_thumbnail($thumbnail, $field, $layout)
+// {
+//   return get_stylesheet_directory_uri() . '/assets/images/layouts/solution_cards.jpg';
+// }
 
-add_filter('acfe/flexible/thumbnail/layout=stats_cards', 'stats_cards_thumbnail', 10, 3);
-function stats_cards_thumbnail($thumbnail, $field, $layout)
-{
-  return get_stylesheet_directory_uri() . '/assets/images/layouts/stats_cards.jpg';
-}
+// add_filter('acfe/flexible/thumbnail/layout=stats_cards', 'stats_cards_thumbnail', 10, 3);
+// function stats_cards_thumbnail($thumbnail, $field, $layout)
+// {
+//   return get_stylesheet_directory_uri() . '/assets/images/layouts/stats_cards.jpg';
+// }
 
-add_filter('acfe/flexible/thumbnail/layout=team', 'team_thumbnail', 10, 3);
-function team_thumbnail($thumbnail, $field, $layout)
-{
-  return get_stylesheet_directory_uri() . '/assets/images/layouts/team.jpg';
-}
+// add_filter('acfe/flexible/thumbnail/layout=team', 'team_thumbnail', 10, 3);
+// function team_thumbnail($thumbnail, $field, $layout)
+// {
+//   return get_stylesheet_directory_uri() . '/assets/images/layouts/team.jpg';
+// }
 
-add_filter('acfe/flexible/thumbnail/layout=testimonial', 'testimonial_thumbnail', 10, 3);
-function testimonial_thumbnail($thumbnail, $field, $layout)
-{
-  return get_stylesheet_directory_uri() . '/assets/images/layouts/testimonial.jpg';
-}
+// add_filter('acfe/flexible/thumbnail/layout=testimonial', 'testimonial_thumbnail', 10, 3);
+// function testimonial_thumbnail($thumbnail, $field, $layout)
+// {
+//   return get_stylesheet_directory_uri() . '/assets/images/layouts/testimonial.jpg';
+// }
 
-add_filter('acfe/flexible/thumbnail/layout=text_image_center', 'text_image_center_thumbnail', 10, 3);
-function text_image_center_thumbnail($thumbnail, $field, $layout)
-{
-  return get_stylesheet_directory_uri() . '/assets/images/layouts/text_image_center.jpg';
-}
+// add_filter('acfe/flexible/thumbnail/layout=text_image_center', 'text_image_center_thumbnail', 10, 3);
+// function text_image_center_thumbnail($thumbnail, $field, $layout)
+// {
+//   return get_stylesheet_directory_uri() . '/assets/images/layouts/text_image_center.jpg';
+// }
